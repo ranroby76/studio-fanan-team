88c7aca295b82ed9c4fd25ae58a7a69d41022b2d
ranroby1 <ranroby76@gmail.com>
2025-07-15 16:44:28 +0000

    what this error wants? FirebaseError: Firebase: Error (auth/requests-fro

---
+++ src/next.config.ts
@@ -0,0 +1,33 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: ['https://9004-firebase-studio-1749223152995.cluster-6frnii43o5blcu522sivebzpii.cloudworkstations.dev'],
+  },
+};
+
+export default nextConfig;
