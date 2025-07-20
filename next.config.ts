42b6dc25d221e82bdb6c84f91645dd49e176a920
ranroby1 <ranroby76@gmail.com>
2025-07-20 13:21:17 +0000

    what this message means?  ✓ Starting...  ✓ Ready in 4.9s  ⚠ Webpack is c

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 
 import type {NextConfig} from 'next';
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})
-
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -37,4 +30,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
