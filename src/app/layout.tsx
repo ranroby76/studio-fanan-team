e89825649c9e66fcd72a2f17d1ed6b4a4a24bb7f
ranroby1 <ranroby76@gmail.com>
2025-07-20 15:11:45 +0000

    9004-firebase-studio-1749223152995.cluster-6frnii43o5blcu522sivebzpii.cl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,6 @@ export const metadata: Metadata = {
     images: [siteConfig.ogImage],
     creator: '@fananteam', // Replace with your actual Twitter handle if you have one
   },
-  manifest: '/manifest.json',
 };
 
 
