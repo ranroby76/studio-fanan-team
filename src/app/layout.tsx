236657580d93f8b08a5a420abfa5249ce8ccf727
ranroby1 <ranroby76@gmail.com>
2025-07-20 13:56:56 +0000

    favicon not changed. is there anything i can manually to force it? somet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,16 +23,11 @@ export const metadata: Metadata = {
   keywords: ["Fanan Team", "VST", "VSTi", "MIDI plugins", "audio plugins", "music production", "synthesizer", "arranger keyboard", "DAW", "Fanan team plugins", "free VST"],
   authors: [{ name: "Fanan Team", url: siteConfig.url }],
   creator: "Fanan Team",
-  manifest: '/manifest.json',
   metadataBase: new URL(siteConfig.url),
   icons: {
-    icon: '/favicon.png',
-    shortcut: '/favicon.png',
-    apple: '/favicon.png',
-    other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: '/favicon.png',
-    },
+    icon: '/favicon.png?v=1',
+    shortcut: '/favicon.png?v=1',
+    apple: '/favicon.png?v=1',
   },
   openGraph: {
     type: 'website',
