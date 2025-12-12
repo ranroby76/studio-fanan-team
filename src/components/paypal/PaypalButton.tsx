6ae332af50a579724ee99a1d7563d6ba753c5775
ranroby1 <ranroby76@gmail.com>
2025-12-12 01:47:42 +0000

    we are missing something. something is blocking paypal: GET https://www.

--- src/components/paypal/PaypalButton.tsx
+++ src/components/paypal/PaypalButton.tsx
@@ -128,7 +128,7 @@ export default function PaypalButton({ price, hostedButtonId }: PaypalButtonProp
   }
 
   return (
-    <PayPalScriptProvider options={{ "client-id": PAYPAL_CLIENT_ID, currency: "USD" }}>
+    <PayPalScriptProvider options={{ "client-id": PAYPAL_CLIENT_ID, currency: "USD", locale: "en_US" }}>
       <div className="w-full max-w-sm flex flex-col items-center gap-4">
         <div className="text-5xl font-bold text-center mb-4 text-primary">${price}</div>
         
